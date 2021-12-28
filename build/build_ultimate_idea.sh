670c688e7e1c276ef6b3fe3e14a2873a98701fa8
Andrey Damintsev <damintsew@gmail.com>
2021-12-29 00:31:47 +0300

    Build script extracted to a separate file

--- build/build_ultimate_idea.sh
+++ build/build_ultimate_idea.sh
@@ -12,7 +12,7 @@ while read VERSION_INDEX; do
 
   IMAGE_NAME=damintsew/indexer-${VERSION_INDEX}${SNAPSHOT_SUFFIX}
   echo "Build image for version $IMAGE_NAME"
-  docker build ../ --build-arg INTELLIJ_VERSION=ideaIU-"${VERSION_INDEX}" --file ../Dockerfile --tag "${IMAGE_NAME}"
+  docker build . --build-arg INTELLIJ_VERSION=ideaIU-"${VERSION_INDEX}" --file Dockerfile --tag "${IMAGE_NAME}"
 
   echo "Push image for version $IMAGE_NAME"
   docker push "$IMAGE_NAME"
