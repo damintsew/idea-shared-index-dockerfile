43c3af93927ffbeb9533aaaf19713f49015c37e5
Andrey Damintsev <damintsew@gmail.com>
2021-12-31 00:21:58 +0300

    Go indexer added

---
+++ build/build_golang_indexer.sh
@@ -0,0 +1,21 @@
+
+filename='./supported_versions.properties'
+echo "Start"
+
+#Checking if need to build release dependency
+if [ -z "$1" ]; then
+    SNAPSHOT_SUFFIX='-snapshot'
+else
+    SNAPSHOT_SUFFIX=""
+fi
+
+while read VERSION_INDEX; do
+
+  IMAGE_NAME=damintsew/indexer-${VERSION_INDEX}${SNAPSHOT_SUFFIX}
+  echo "Build image for version $IMAGE_NAME"
+  docker build . --build-arg INTELLIJ_VERSION=ideaIU-"${VERSION_INDEX}" --file images/GO-indexer-Dockerfile --tag "${IMAGE_NAME}"
+
+  echo "Push image for version $IMAGE_NAME"
+  docker push "$IMAGE_NAME"
+
+done < "$filename"
