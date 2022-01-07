e01cdd07eec02f213ad0f10db05a6b11800ac03f
Andrey Damintsev <damintsew@gmail.com>
2022-01-08 00:12:43 +0300

    try added ghcr.io

--- build/build_ultimate_idea.sh
+++ build/build_ultimate_idea.sh
@@ -3,17 +3,28 @@ filename='./supported_versions.properties'
 echo "Start"
 
 #Checking if need to build release dependency
-if [ -z "$1" ]; then
+
+
+#Set Docker provider based on input params
+#if [ -z "$2" ]; then
+#    DOCKER_PROVIDER='docker.io'
+#else
+#    DOCKER_PROVIDER="ghcr.io"
+#fi
+
+while getopts "v:p:" opt
+do
+   case "$opt" in
+      v ) SNAPSHOT_SUFFIX="$OPTARG" ;;
+      p ) DOCKER_PROVIDER="$OPTARG" ;;
+      ? ) echo "help" # helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+if [ -z "${SNAPSHOT_SUFFIX}" ]; then
     SNAPSHOT_SUFFIX='-snapshot'
 else
     SNAPSHOT_SUFFIX=""
-fi
-
-#Set Docker provider based on input params
-if [ -z "$2" ]; then
-    DOCKER_PROVIDER='docker.io'
-else
-    DOCKER_PROVIDER="ghcr.io"
 fi
 
 while read VERSION_INDEX; do
@@ -21,10 +32,9 @@ while read VERSION_INDEX; do
   IMAGE_NAME=${DOCKER_PROVIDER}/damintsew/indexer-${VERSION_INDEX}${SNAPSHOT_SUFFIX}
   echo "Build image for version $IMAGE_NAME"
   docker build . --build-arg INTELLIJ_VERSION=ideaIU-"${VERSION_INDEX}" --file Dockerfile \
-   --tag "${IMAGE_NAME}" --tag ghcr.io/"${IMAGE_NAME}"
+   --tag "${IMAGE_NAME}" --tag "${IMAGE_NAME}"
 
   echo "Push image for version $IMAGE_NAME"
-#  docker push "$IMAGE_NAME"
-  docker push ghcr.io/"$IMAGE_NAME"
+  docker push "$IMAGE_NAME"
 
 done < "$filename"
