ed107496caf92dd4dc529980eec2eaf57685f1b7
Andrey Damintsev <damintsew@gmail.com>
2022-01-07 23:53:53 +0300

    try added ghcr.io

--- build/build_ultimate_idea.sh
+++ build/build_ultimate_idea.sh
@@ -9,9 +9,16 @@ else
     SNAPSHOT_SUFFIX=""
 fi
 
+#Set Docker provider based on input params
+if [ -z "$2" ]; then
+    DOCKER_PROVIDER='docker.io'
+else
+    DOCKER_PROVIDER="ghcr.io"
+fi
+
 while read VERSION_INDEX; do
 
-  IMAGE_NAME=damintsew/indexer-${VERSION_INDEX}${SNAPSHOT_SUFFIX}
+  IMAGE_NAME=${DOCKER_PROVIDER}/damintsew/indexer-${VERSION_INDEX}${SNAPSHOT_SUFFIX}
   echo "Build image for version $IMAGE_NAME"
   docker build . --build-arg INTELLIJ_VERSION=ideaIU-"${VERSION_INDEX}" --file Dockerfile \
    --tag "${IMAGE_NAME}" --tag ghcr.io/"${IMAGE_NAME}"
